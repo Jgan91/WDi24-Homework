eb1dcc6819fbd7e7d5a5b88258c014a418861b81
JustinGan <jgan91@gmail.com>
2017-12-11 16:24:44 +1100

    fetchImages updates FlickrSearch's state

--- justin_gan/week_08/day_01/flickr-search/src/components/FlickrSearch.js
+++ justin_gan/week_08/day_01/flickr-search/src/components/FlickrSearch.js
@@ -1,11 +1,50 @@
 import React, { PureComponent } from 'react';
+import jsonp from 'jsonp-es6'
 
 class FlickrSearch extends PureComponent {
+  constructor() {
+    super();
+    this.state = { images: [] };
+    this.fetchImages = this.fetchImages.bind( this );
+  }
+
+  fetchImages(q) {
+    console.log( 'search for', q, this );
+
+    const flickrURL = 'https://api.flickr.com/services/rest/?jsoncallback=?';
+    const flickrParams = {
+      method: 'flickr.photos.search',
+      api_key: '2f5ac274ecfac5a455f38745704ad084',
+      text: q,
+      format: 'json',
+      per_page: 500
+    }
+
+    const generateURL = function (photo) {
+      return [
+        'http://farm',
+        photo.farm,
+        '.static.flickr.com/',
+        photo.server,
+        '/',
+        photo.id,
+        '_',
+        photo.secret,
+        '_q.jpg' // Change "q" for different sizes
+      ].join('');
+    }
+
+    jsonp( flickrURL, flickrParams, { callback: 'jsoncallback' } ).then( ( results ) => {
+      const images = results.photos.photo.map( generateURL );
+      this.setState( { images: images } );
+    } );
+  }
+
   render() {
     return (
       <React.Fragment>
         <h1>FlickrSearch</h1>
-        <SearchForm />
+        <SearchForm onSubmit={ this.fetchImages }/>
         <Gallery />
       </React.Fragment>
     );
@@ -13,26 +52,26 @@ class FlickrSearch extends PureComponent {
 }
 
 class SearchForm extends PureComponent {
-  constructor() {
-    super();
+  constructor( props ) {
+    super( props );
     this.state = { query: '' };
-    this._handleClick = this._handleClick.bind( this );
-    this._handleChange = this._handleChange.bind( this );
+    this._handleSubmit = this._handleSubmit.bind( this );
+    this._handleInput = this._handleInput.bind( this );
   }
 
-  _handleClick(e) {
+  _handleSubmit(e) {
     e.preventDefault();
-    console.log( 'submitted', this.state.query );
+    this.props.onSubmit( this.state.query );
   }
 
-  _handleChange(e) {
+  _handleInput(e) {
     this.setState( { query: e.target.value } );
   }
 
   render() {
     return (
-      <form onSubmit={ this._handleClick }>
-        <input type="search" placeholder="Some text..." onChange={ this._handleChange } />
+      <form onSubmit={ this._handleSubmit }>
+        <input type="search" placeholder="Some text..." onChange={ this._handleInput } />
         <input type="submit" value="Go" />
       </form>
     )
